fc5543e42f49093c54ac7df32e8c0e74032d503c
cleosyuu <cleo.syu@gmail.com>
2021-11-09 05:09:54 +0800

    add: stylelint setting

---
+++ stylelint.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: [
+    'stylelint-config-recommended-scss',
+    'stylelint-config-recess-order',
+    'stylelint-config-recommended-vue'
+  ],
+  rules: {
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+    'function-calc-no-unspaced-operator': true // calc 空白間距
+  },
+  ignoreFiles: [
+    '.vscode/**',
+    'dist/**',
+    'node_modules/**',
+    '**/*.js',
+    '**/*.ts'
+  ]
+}
