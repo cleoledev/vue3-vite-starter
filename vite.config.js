b095395d01fd58c9e18767d7bbce260707efb194
cleosyuu <cleo.syu@gmail.com>
2021-11-09 15:58:39 +0800

    add: vite-plugin-legacy setting

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    legacy({
+      targets: ['ie >= 11'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+    })
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
